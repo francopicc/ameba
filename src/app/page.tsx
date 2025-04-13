9a827d014299f9ceefc6a21c747f00007070f26b
breakerjs <francopiccirilli51@gmail.com>
2025-04-12 22:32:33 -0300

    Passing Session Errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+"use client"
+import { getSession } from "@/lib/session";
+import { useState, useEffect } from "react";
+import { Session } from "@supabase/supabase-js";
 
 export default function Home() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    async function fetchSession() {
+      const session = await getSession();
+      setSession(session);
+    }
+    fetchSession();
+  }, []);
+  
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <p>Testing</p>
+      <p>Welcome {session?.user?.email}</p>
     </div>
   );
 }
---
+++ src/lib/session.ts
@@ -0,0 +1,14 @@
+'use server'
+import { createClient } from "@/utils/supabase/server";
+
+export async function getSession() {
+  const supabase = await createClient();
+  try {
+    const { data: { session }, error } = await supabase.auth.getSession();
+    if (error) throw error;
+    return session;
+  } catch (error) {
+    console.error('Error:', error);
+    return null;
+  }
+}
